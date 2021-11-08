0d04dd4de05e1a8f6c168e4a5f3ca58a6849cdf4
Giovanna Rosa <gmarcaro@gmail.com>
2021-11-08 16:16:08 -0300

    js

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Users should be able to:
 - See hover states for all interactive elements on the page
 - Receive an error message when the `form` is submitted if:
   - Any `input` field is empty. The message for this error should say _"[Field Name] cannot be empty"_
-  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say _"Looks like this is not an email"_
+  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`. The message for this error should say _"Looks like this is not an email"_
 
 ### Links
 
---
+++ script.js
@@ -0,0 +1,77 @@
+const fields = document.querySelectorAll('[required]')
+
+function ValidateField(field) {
+  function verifyErrors() {
+    let foundError = false
+
+    for (let error in field.validity) {
+      if (field.validity[error] && !field.validity.valid) {
+        foundError = error
+      }
+    }
+    return foundError
+  }
+
+  function customMessage(typeError) {
+    const messages = {
+      text: {
+        valueMissing: '[Field Name] cannot be empty'
+      },
+      password: {
+        valueMissing: 'Password cannot be empty'
+      },
+      email: {
+        valueMissing: 'Email cannot be empty',
+        typeMismatch: 'Looks like this is not an email'
+      }
+    }
+    return messages[field.type][typeError]
+  }
+
+  function setCustomMessage(message) {
+    const spanError = field.parentNode.querySelector('span.error')
+
+    if (message) {
+      spanError.classList.add('active')
+      spanError.innerHTML = message
+    } else {
+      spanError.classList.remove('active')
+      spanError.innerHTML = ''
+    }
+  }
+
+  return function () {
+    const error = verifyErrors()
+
+    if (error) {
+      const message = customMessage(error)
+
+      field.style.borderColor = 'var(--red)'
+      setCustomMessage(message)
+    } else {
+      setCustomMessage()
+    }
+  }
+}
+
+function customValidation(event) {
+  const field = event.target
+  const validation = ValidateField(field)
+
+  validation()
+}
+
+for (field of fields) {
+  field.addEventListener('invalid', event => {
+    event.preventDefault()
+
+    customValidation(event)
+  })
+  field.addEventListener('blur', customValidation)
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+  console.log('enviar o formulário')
+
+  event.preventDefault()
+})
